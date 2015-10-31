b039ee23b676fab1155a84d65aa22e9f4ec6c6ec
= <=>
2015-10-31 20:43:08 +0000

    last version of routes and url_for

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return "Hello World!"
+@app.route('/user/<username>')
+def show_user_profile(username):
+  return "User: " + str(username)
+
+
+@app.route('/')
+def show_url_for():
+  return url_for('show_user_profile', username='Clifford')
 
 if __name__ == '__main__':
+  app.debug = True
   app.run(host='0.0.0.0')
