077be252367aec5de09adc459873f305a2a84a51
= <=>
2015-11-01 20:47:38 +0000

    Included redirects and variable passing

--- hello.py
+++ hello.py
@@ -1,15 +1,32 @@
-from flask import Flask, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 app = Flask(__name__)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-  return "User: " + str(username)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  error = None
+  if request.method == 'POST':
+    if valid_login(request.form.get('username'),
+		   request.form.get('password')
+		   ):
+      flash("Succesfully logged in")
+      flash("Thanks for coming")
+      return redirect(url_for('welcome', username=request.form.get('username')))
+    else:
+      error = "Incorrect username and password"
+  return render_template('login.html', error=error)
 
+@app.route('/welcome/<username>')
+def welcome(username):
+  return render_template('welcome.html', username=username)
 
-@app.route('/')
-def show_url_for():
-  return url_for('show_user_profile', username='Clifford')
+def valid_login(username, password):
+  #checks on the db if the username and password are correct
+  if username == password:
+    return True
+  else:
+    return False
 
 if __name__ == '__main__':
+  app.secret_key = 'SuperSecretPassword1234!@#$'
   app.debug = True
   app.run(host='0.0.0.0')
